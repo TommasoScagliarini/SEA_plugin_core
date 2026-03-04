9c1e0e4a07e818445734d004ac44a3123c78e7e3
TommasoScagliarini <tommaso.scagliarini@studio.unibo.it>
2026-03-04 12:01:08 +0100

    initModel function

--- ForwardDynamics.cpp
+++ ForwardDynamics.cpp
@@ -1,6 +1,7 @@
 #include <OpenSim/OpenSim.h>
 #include "SeriesElasticActuator.h"
 #include "SEATrackingController.h"
+#include "InitModel.h"
 
 using namespace OpenSim;
 using namespace SimTK;
@@ -14,8 +15,9 @@ int main() {
         // Real model with SEA
         Model model("C:\\Users\\tomma\\Desktop\\Opensim OMNIBUS\\SEA-plugin-OpenSim - core\\Adjusted_sea.osim");
         
-        double Kp = 100; 
-        double Kd = 2.5; 
+        double Kp = 1000; 
+        double Kd = 20; 
+        //double Kd = 2*sqrt(Kp);
         
         /* First argument: Actuation_force.sto file of the "ideal model" (CMC output)
         Second argument: Name of the actuator in the CMC file (e.g., "reserve_pros_knee_angle")
@@ -37,81 +39,8 @@ int main() {
         double startTime = 4.26;
         s.setTime(startTime);
 
-        const CoordinateSet& coordSet = model.getCoordinateSet();
-        Array<double> stateValues;
-        stateValues.setSize(coordSet.getSize());
-        KinRefFile.getDataAtTime(startTime, coordSet.getSize(), stateValues);
 
-        Array<std::string> colNames = KinRefFile.getColumnLabels();
-
-        /* for (int col = 1; col < colNames.getSize(); ++col) { // col=0 è "time"
-            std::string coordName = colNames[col];
-            
-            // Rimuovi eventuale suffisso "/value" se presente
-            auto slashPos = coordName.rfind('/');
-            if (slashPos != std::string::npos) {
-                coordName = coordName.substr(slashPos + 1);
-            }
-            
-            // Controlla che la coordinata esista nel modello
-            if (!model.getCoordinateSet().contains(coordName)) {
-                cout << "[WARN] Coordinata '" << coordName << "' nel .sto non trovata nel modello. Skippata." << endl;
-                continue;
-            }
-            
-            const Coordinate& coord = model.getCoordinateSet().get(coordName);
-            double val = stateValues[col - 1]; // stateValues non include la colonna time
-            
-            // Conversione gradi -> radianti per coordinate rotazionali
-            if (coord.getMotionType() == Coordinate::Rotational) {
-                val = SimTK::convertDegreesToRadians(val);
-            }
-            
-            coord.setValue(s, val);
-            cout << "[INIT] " << coordName << " = " << val << " rad" << endl;
-        } */
-        
-        for (int col = 1; col < colNames.getSize(); ++col) { // col=0 è "time"
-            std::string coordName = colNames[col];
-            
-            // Rimuovi eventuale suffisso "/value" se presente
-            auto slashPos = coordName.rfind('/');
-            if (slashPos != std::string::npos) {
-                coordName = coordName.substr(slashPos + 1);
-            }
-            
-            // Controlla che la coordinata esista nel modello
-            if (!model.getCoordinateSet().contains(coordName)) {
-                cout << "[WARN] Coordinata '" << coordName << "' nel .sto non trovata nel modello. Skippata." << endl;
-                continue;
-            }
-            
-            // ATTENZIONE: Rimosso 'const' per poter usare setLocked
-            const Coordinate& coord = model.getCoordinateSet().get(coordName);
-            double val = stateValues[col - 1]; // stateValues non include la colonna time
-            
-            // Conversione gradi -> radianti per coordinate rotazionali
-            if (coord.getMotionType() == Coordinate::Rotational) {
-                val = SimTK::convertDegreesToRadians(val);
-            }
-            
-            // 1. Imposta il valore nel sistema
-            coord.setValue(s, val);
-
-            // 2. BLOCCO DELLE COORDINATE: Blocca tutto tranne il ginocchio con il SEA
-            if (coordName != "pros_knee_angle") {
-                coord.setLocked(s, true);
-            }
-            
-            // 3. Stampa a schermo indicando se è stata bloccata
-            cout << "[INIT] " << coordName << " = " << val << " rad" 
-                 << (coord.getLocked(s) ? " (LOCKED)" : "") << endl;
-        }
-
-        auto& sea = model.getComponent<SeriesElasticActuator>("forceset/SEA");
-        double knee_angle = model.getCoordinateSet().get("pros_knee_angle").getValue(s);
-        sea.setStateVariableValue(s, "motor_angle", knee_angle);
-        sea.setStateVariableValue(s, "motor_speed", 0.0);
+        initializeModel(model, s, KinRefFile, startTime);
         
         model.assemble(s);
         Manager manager(model);
---
+++ InitModel.h
@@ -0,0 +1,67 @@
+#ifndef INITMODEL_H
+#define INITMODEL_H
+
+#include <OpenSim/OpenSim.h>
+#include "SeriesElasticActuator.h"
+#include <iostream>
+
+
+using namespace OpenSim;
+using namespace SimTK;
+using namespace std;
+
+void initializeModel(Model& model, State& s, Storage KinRefFile, double startTime);
+
+void initializeModel(Model& model, State& s, Storage KinRefFile, double startTime) {
+    const CoordinateSet& coordSet = model.getCoordinateSet();
+    Array<double> stateValues;
+    stateValues.setSize(coordSet.getSize());
+    KinRefFile.getDataAtTime(startTime, coordSet.getSize(), stateValues);
+
+    Array<std::string> colNames = KinRefFile.getColumnLabels();
+
+    for (int col = 1; col < colNames.getSize(); ++col) { // col=0 è "time"
+            std::string coordName = colNames[col];
+            
+            // Rimuovi eventuale suffisso "/value" se presente
+            auto slashPos = coordName.rfind('/');
+            if (slashPos != std::string::npos) {
+                coordName = coordName.substr(slashPos + 1);
+            }
+            
+            // Controlla che la coordinata esista nel modello
+            if (!model.getCoordinateSet().contains(coordName)) {
+                cout << "[WARN] Coordinata '" << coordName << "' nel .sto non trovata nel modello. Skippata." << endl;
+                continue;
+            }
+            
+            // ATTENZIONE: Rimosso 'const' per poter usare setLocked
+            const Coordinate& coord = model.getCoordinateSet().get(coordName);
+            double val = stateValues[col - 1]; // stateValues non include la colonna time
+            
+            // Conversione gradi -> radianti per coordinate rotazionali
+            if (coord.getMotionType() == Coordinate::Rotational) {
+                val = SimTK::convertDegreesToRadians(val);
+            }
+            
+            // 1. Imposta il valore nel sistema
+            coord.setValue(s, val);
+
+            // 2. BLOCCO DELLE COORDINATE: Blocca tutto tranne il ginocchio con il SEA
+            if (coordName != "pros_knee_angle") {
+                coord.setLocked(s, true);
+            }
+            
+            // 3. Stampa a schermo indicando se è stata bloccata
+            cout << "[INIT] " << coordName << " = " << val << " rad" 
+                 << (coord.getLocked(s) ? " (LOCKED)" : "") << endl;
+        }
+
+        auto& sea = model.getComponent<SeriesElasticActuator>("forceset/SEA");
+        double knee_angle = model.getCoordinateSet().get("pros_knee_angle").getValue(s);
+        sea.setStateVariableValue(s, "motor_angle", knee_angle);
+        sea.setStateVariableValue(s, "motor_speed", 0.0);
+    
+}
+
+#endif
